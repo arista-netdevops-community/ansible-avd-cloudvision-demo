9cbacf68ef91a5835a6e210017e9f00c8aa6037e
Thomas Grimonet <tgrimonet@arista.com>
2020-02-11 11:45:17 +0100

    Update script with new AVD structure

--- repository-cleanup.sh
+++ repository-cleanup.sh
@@ -3,7 +3,9 @@
 echo 'Cleaning up repository from previous run'
 echo '  - delete content in documentation/'
 find documentation/ -name "*.md" -type f -delete
-echo '  - delete content in generated_vars/'
-find generated_vars -name "*.yml" -type f -delete
-echo '  - delete content in intended_configs/'
-find intended_configs -name "*.cfg" -type f -delete
+echo '  - delete content in documentation/'
+find documentation/ -name "*.csv" -type f -delete
+echo '  - delete content in intended/structured_configs/'
+find intended/structured_configs/ -name "*.yml" -type f -delete
+echo '  - delete content in intended/configs/'
+find intended/configs/ -name "*.cfg" -type f -delete
